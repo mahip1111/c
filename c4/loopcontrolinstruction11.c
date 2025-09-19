00a393d047234d827b9a9b53d5978f828b8c5451
mahip1111 <guptamanavendra6@gmail.com>
2025-09-19 08:34:05 +0530

    Update loopcontrolinstruction11.c

--- c4/loopcontrolinstruction11.c
+++ c4/loopcontrolinstruction11.c
@@ -8,3 +8,9 @@ int main(){
         
     return 0;
 }
+// In your case:- 
+1. you did initialization process which you are clear
+2. you set the condition as i means i ha jo agr true hai toh hi loop continue hoga,
+3. you did decrement.
+now since the loop is decrementing the time will come when 1 become 0 and when the i will become 0 the condition i itself will go false and thus it exits the loop 
+That is why you are getting the ans as 12-1.
